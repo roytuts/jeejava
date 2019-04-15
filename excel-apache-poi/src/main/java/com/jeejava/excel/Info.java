04bb972e9cefa357879d52836f1ac921fbed62ef
jeejava roytuts <projwebinfo@gmail.com>
2019-04-15 16:24:24 +0530

    Add files via upload

---
+++ excel-apache-poi/src/main/java/com/jeejava/excel/Info.java
@@ -0,0 +1,57 @@
+package com.jeejava.excel;
+
+public class Info {
+
+	private String name;
+	private String mobile;
+	private String phone;
+	private String permAddress;
+	private String commAddress;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPermAddress() {
+		return permAddress;
+	}
+
+	public void setPermAddress(String permAddress) {
+		this.permAddress = permAddress;
+	}
+
+	public String getCommAddress() {
+		return commAddress;
+	}
+
+	public void setCommAddress(String commAddress) {
+		this.commAddress = commAddress;
+	}
+
+	@Override
+	public String toString() {
+		return "Info [Name=" + name + ", Mobile=" + mobile + ", Phone=" + phone + ", Permanent Address=" + permAddress
+				+ ", Communication Address=" + commAddress + "]";
+	}
+
+}
